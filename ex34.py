210fb1d23ff32d72647d4979dd052039bec04d41
Ben Castan <ben@castanfamily.com>
2017-01-30 15:38:06 +1100

    end of ex35

---
+++ ex34.py
@@ -0,0 +1,22 @@
+#ex34.py
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+"""
+Animal at 1 = peacock
+Third animal = peacock
+First animal = bear
+The animal at 3 = kangaroo
+The fifth animal = platypus
+The animal at 2 = peacock
+The sixth animal = platypus
+The animal at 4 = whale
+"""
+print animals[1]
+print animals[2]
+print animals[0]
+print animals[2]
+print animals[4]
+print animals[1]
+print animals[5]
+print animals[3]
